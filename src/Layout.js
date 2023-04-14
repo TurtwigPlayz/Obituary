00ee720ea4044536f8b78f63a90bad4e9ca499e0
TurtwigPlayz <63518111+TurtwigPlayz@users.noreply.github.com>
2023-04-14 09:12:02 -0600

    Update Layout.js

--- src/Layout.js
+++ src/Layout.js
@@ -1,38 +1,47 @@
 import React, { useEffect, useState } from 'react';
 import App from './App';
 import Create from './Create';
-function Layout() {
 
+function Layout() {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   const [obituaries, setObituaries] = useState([]);
-  
+  const [created, setCreated] = useState(false); // add this state variable
+
   const getObituaries = () => {
-    console.log("Shit");
     fetch("https://bhakaiz7jjw6pjou7qmy6gybqy0ohbrr.lambda-url.ca-central-1.on.aws/",{
       method:"GET",
     })
-      .then((response)=>{return response.json();})
-        .then((data)=>{setObituaries(data);})
-        
+    .then((response)=>{return response.json();})
+    .then((data)=>{setObituaries(data);})
   }
 
   useEffect(() => {
     getObituaries();
   }, [])
-  
-    return (
-      <>
+
+  const handleCreate = () => {
+    setCreated(true); // set created to true on successful creation
+  }
+
+  return (
+    <>
       <nav className ="navbar">
         <div>&nbsp;</div>
         <div className="navbar-brand" style={{position: 'absolute', left: '46.5%'}}>The Last Show</div>
         <button type="button" className="btn btn-outline-secondary" onClick={handleShow}>+ New Obituary</button>
       </nav>
-      <Create show={show} handleClose={handleClose} getObituaries={getObituaries}/>
-      <App obituaries={obituaries}/>
-      </>
-    );
+      {created ? ( // conditionally render the Create component
+        <App obituaries={obituaries}/> // show the app instead of create
+      ) : (
+        <Create show={show} handleClose={handleClose} getObituaries={getObituaries} handleCreate={handleCreate}/>
+      )}
+    </>
+  );
+}
+
+export default Layout;
   }
   
   export default Layout;
