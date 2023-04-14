82e0c7b35989c222d3918be0c8f6e56e1114f484
TurtwigPlayz <63518111+TurtwigPlayz@users.noreply.github.com>
2023-04-14 08:58:40 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import Obituaries from "./Obituaries";
+import React from 'react';
+import Obituaries from './Obituaries';
 
 function App({obituaries}) {
-  return obituaries.length > 0 ? (
-    <div className="d-flex align-items-center flex-wrap">
-      {obituaries.map((data, index) => (
-        <Obituaries data={data} key={`obituary-item-${index}`}/>
-      ))}
-    </div>
-  ) : (
-    <div style={{width:"100%", height:"100%", textAlign: "center"}}>
-    <p id="no-note-yet" className="mx-auto my-auto font-weight-bold display-5 text-muted">No Obituaries Yet</p>
+  return (
+    <div className="container mt-4">
+      <div className="row row-cols-4 g-4">
+        {obituaries.map((obituary, index) => (
+          <div className="col" key={index}>
+            <Obituaries data={obituary} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
